8d0b33a65f87cdd773bb9212a82588cee1c6b495
BraveKrish <krishparajuli57@gmail.com>
2025-01-12 18:02:26 +0545

    services section and about content changes

--- src/components/homeAbout/HomeAboutContent.tsx
+++ src/components/homeAbout/HomeAboutContent.tsx
@@ -7,45 +7,38 @@ const HomeAboutContent = () => {
     <>
       <div>
         <p className="text-2xl uppercase tracking-[0.2em] text-main-color">
-          welcome
+          Welcome to {" "}
+          <span className="bg-main-color inline-block text-white text-[14px] relative bottom-3 py-3 px-5 rounded-[30px]">
+            SSN Constructions
+          </span>{" "}
         </p>
       </div>
       <div className="py-5">
         <h1 className="text-[#0E121D] text-5xl font-extrabold">
-        Committed <span className="text-main-color">to</span>{" "}
-          <span className="bg-main-color inline-block text-white text-[14px] relative bottom-3 py-3 px-5 rounded-[30px]">
-            SSS Construction
-          </span>{" "}
-          <br />
-          <span className="text-main-color">Excellence in</span> Every <br />
-          Build Work with SSN
+          Your Trusted Partner in <span className="text-main-color">Construction</span> Excellence
         </h1>
       </div>
+
       <div>
         <p className="text-[#64656a] text-base tracking-wider leading-[28px] text-justify">
-          We are the construction, engineering, and management professionals
-          having years of experiences in the industry. Our vibrant and dynamic
-          culture, we proudly called it SSNian culture, prioritizes fun at work,
-          fostering collaboration, creativity, and job satisfaction. We
-          celebrate diversity and inclusion, valuing unique talents and
-          perspectives that drive innovation.
+          At SSN Builders, our journey is defined by a relentless pursuit of top-class quality and a commitment to excellence in every project we undertake. With a vision to become pioneers in the construction industry, we continuously innovate and adapt to the latest technologies and methodologies, ensuring that our work not only meets but exceeds industry standards.
+          Our seamless services are designed to provide a hassle-free experience for our clients, from the initial consultation to the final handover. We believe that every project is unique, and we offer personalized attention to ensure that each client’s specific needs and preferences are met with precision and care.
+          Community focus is at the heart of our operations. We understand the importance of building not just structures, but also relationships and trust within the communities we serve. SSN Builders is here to provide unparalleled expertise. Together, let's create spaces that inspire and endure your dream home, and commercial spaces or embark on a large-scale construction project.
+
+
         </p>
         <p className="text-[#64656a] text-base tracking-wider leading-[28px] pt-4 text-justify">
-          Our inclusive environment empowers everyone to contribute their best.
-          Giving back to the community is deeply ingrained in our culture.
-          Through philanthropic initiatives and partnerships, we make a positive
-          impact. Join us on this exciting journey to build exceptional projects
-          and create a nurturing environment that values every individuals
-          contribution. We highly believe that “Together, We Make a Difference”.
+          At SSN Builders, we are more than just a construction company; we are a team of dedicated professionals who are passionate about creating spaces that inspire and endure. Our commitment to excellence drives us to deliver projects that stand the test of time, reflecting our unwavering dedication to quality and innovation.
+          Join us on our journey as we strive to set new benchmarks in the construction industry, one project at a time. Thank you for considering SSN Builders. We look forward to work with you and turn your vision into reality.
         </p>
       </div>
-      <div className="block pt-14">
+      {/* <div className="block pt-14">
         <Image
           src={signature}
           alt="Signature Image"
           className="max-w-full h-full object-cover"
         />
-      </div>
+      </div> */}
     </>
   );
 };
--- src/pages/about/AboutInfo.tsx
+++ src/pages/about/AboutInfo.tsx
@@ -11,10 +11,10 @@ const AboutInfo = () => {
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 place-items-center gap-5">
           <div>
-            <Image 
-            src={about_1}
-            alt="about Image"
-            className="max-w-full h-full object-cover"
+            <Image
+              src={about_1}
+              alt="about Image"
+              className="max-w-full h-full object-cover"
             />
           </div>
           <div>
@@ -26,12 +26,11 @@ const AboutInfo = () => {
               Your Dream
             </h2>
             <p className="text-[#1D1D1D] py-4">
-              Many modern construction companies focus on sustainable building
-              practices, incorporating eco-friendly material energy-efficient
-              systems and environmental conscious designs to reduce the
-              environmental impact of their projects.
+              Join us on our journey as we strive to set new benchmarks in the construction industry, one project at a time. Thank you for considering SSN Builders. We look forward to work with you and turn your vision into reality.
             </p>
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4">
+              {/* below services card start */}
+              {/* Card 1 */}
               <div className="bg-[#F8F8F8] flex flex-col gap-2 py-6 px-6">
                 <Image
                   src={worker}
@@ -42,25 +41,87 @@ const AboutInfo = () => {
                   Quality And Expert Team Members
                 </h6>
                 <p className="text-[#1D1D1D] text-sm font-medium">
-                  Initial phase involving project conceptualization, design, and
-                  obtaining necessary.
+                  Initial phase involving project conceptualization, design, and obtaining necessary.
                 </p>
               </div>
+
+              {/* Card 2 */}
               <div className="bg-[#F8F8F8] flex flex-col gap-2 py-6 px-6">
                 <Image
                   src={certificate}
-                  alt="Worker Image"
+                  alt="Certificate Image"
                   className="w-[60px] h-[60px] object-cover"
                 />
                 <h6 className="text-title-color text-lg font-semibold">
-                  Certified & Awards winner
+                  Certified & Awards Winner
                 </h6>
                 <p className="text-[#1D1D1D] text-sm font-medium">
-                  Initial phase involving project conceptualization, design, and
-                  obtaining necessary.
+                  Initial phase involving project conceptualization, design, and obtaining necessary.
+                </p>
+              </div>
+
+              {/* Card 3 */}
+              <div className="bg-[#F8F8F8] flex flex-col gap-2 py-6 px-6">
+                <Image
+                  src={worker}
+                  alt="Tools Image"
+                  className="w-[60px] h-[60px] object-cover"
+                />
+                <h6 className="text-title-color text-lg font-semibold">
+                  Advanced Tools & Technology
+                </h6>
+                <p className="text-[#1D1D1D] text-sm font-medium">
+                  Utilizing state-of-the-art tools and modern technologies to ensure quality outcomes.
+                </p>
+              </div>
+
+              {/* Card 4 */}
+              <div className="bg-[#F8F8F8] flex flex-col gap-2 py-6 px-6">
+                <Image
+                  src={worker}
+                  alt="Support Image"
+                  className="w-[60px] h-[60px] object-cover"
+                />
+                <h6 className="text-title-color text-lg font-semibold">
+                  24/7 Customer Support
+                </h6>
+                <p className="text-[#1D1D1D] text-sm font-medium">
+                  Always ready to assist our clients with any queries or project updates.
+                </p>
+              </div>
+
+              {/* Card 5 */}
+              <div className="bg-[#F8F8F8] flex flex-col gap-2 py-6 px-6">
+                <Image
+                  src={certificate}
+                  alt="Experience Image"
+                  className="w-[60px] h-[60px] object-cover"
+                />
+                <h6 className="text-title-color text-lg font-semibold">
+                  Decades of Experience
+                </h6>
+                <p className="text-[#1D1D1D] text-sm font-medium">
+                  With years of experience in the construction industry, we deliver excellence.
+                </p>
+              </div>
+
+              {/* Card 6 */}
+              <div className="bg-[#F8F8F8] flex flex-col gap-2 py-6 px-6">
+                <Image
+                  src={worker}
+                  alt="Safety Image"
+                  className="w-[60px] h-[60px] object-cover"
+                />
+                <h6 className="text-title-color text-lg font-semibold">
+                  Commitment to Safety
+                </h6>
+                <p className="text-[#1D1D1D] text-sm font-medium">
+                  Safety is our top priority, ensuring a secure and healthy work environment.
                 </p>
               </div>
             </div>
+            {/* below services card end */}
+
           </div>
         </div>
       </div>
